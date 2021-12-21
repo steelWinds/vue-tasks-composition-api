4974aa3dca6a0eb3e1f3141809a3dbeb89cd7084
ninja <kirillsurov0@gmail.com>
2021-12-22 06:26:13 +0900

    feat(templates, vite): add vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { minifyHtml, injectHtml } from 'vite-plugin-html';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
     return {
         plugins: [
-            vue()
+            vue(),
+            minifyHtml(),
+            injectHtml({
+                data: {
+                    title: 'CompositionApi and Vite'
+                }
+            })
         ],
 
-        base: command === 'serve' ? '/' : '/vue-tasks-composition-api'
-    }
-})
+        base: command === 'serve' ? '/' : '/vue-tasks-composition-api/'
+    };
+});
