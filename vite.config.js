f062c883819f357b25313509f68670a74ec2621e
ninja <kirillsurov0@gmail.com>
2021-12-22 03:52:50 +0900

    fix(docs): fix base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command }) => {
+    if (command === 'serve') {
+        return {
+            plugins: [
+                vue()
+            ],
+            base: '/'
+        }
+    } else {
+        return {
+            plugins: [
+                vue()
+            ],
+            base: '/vue-tasks-composition-api/'
+        }
+    }
 })
